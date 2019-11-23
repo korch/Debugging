8c9959a002c236e2bee4302447ecdd7e1d540783
Egor Sobalevksy <e.sobalevsky@gmail.com>
2019-11-23 17:56:37 +0300

    Add debugging task
    Add debugging task

---
+++ Debugging.StanovimsiaHackerami.GenerimKey/KeyGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace Debugging.StanovimsiaHackerami.GenerimKey
+{
+    internal class KeyGenerator
+    {
+        public string Key()
+        {
+            var netInterface = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault();
+
+            if (netInterface == null)
+                Console.WriteLine("Bedaa, bratan, megom v magaz za network interface'om'");
+
+            var bufNetworkInterface = netInterface.GetPhysicalAddress().GetAddressBytes();
+            var bufDate = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+            var count = Math.Min(bufNetworkInterface.Length, bufDate.Length);
+
+            var list = new List<int>(count);
+
+            for (var i = 0; i < count; i++)
+            {
+                var partOfKey = (bufDate[i] ^ bufNetworkInterface[i]) * 10;
+                list.Add(partOfKey);
+            }
+
+            return string.Join("-", list);
+        }
+    }
+}
---
+++ Debugging.StanovimsiaHackerami.GenerimKey/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Debugging.StanovimsiaHackerami.GenerimKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keyGenerator = new KeyGenerator();
+            var key = keyGenerator.Key();
+
+            Console.WriteLine($"Vot tvoi klu4:{key}");
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
